4eaa1f5d5abffd7446206ab2d90797d8be68a132
Louis Schnellbach <louis.schnellbach@gmail.com>
2015-10-08 09:40:19 +0200

    Added some basic Unit Tests

--- unittests/main.cpp
+++ unittests/main.cpp
@@ -35,6 +35,7 @@
 #include <iostream>
 #include <Muon/System/Log.hpp>
 #include <Muon/Meta/MetaDatabase.hpp>
+#include <Muon/Memory/PoolAllocator.hpp>
 
 class LogFile : public muon::system::ILogImpl
 {
@@ -75,6 +76,23 @@ public:
 	protected:
 		std::ofstream _file;
 };
+	
+struct UnitTestObject
+{
+	static const muon::u32 ConstructedValue = 64;
+	static const muon::u32 DestructedValue = 42;
+	muon::u32 value;
+
+	UnitTestObject()
+	{
+		value = ConstructedValue;
+	}
+
+	~UnitTestObject()
+	{
+		value = DestructedValue;
+	}
+};
 
 int main(int argc, char** argv)
 {
@@ -83,7 +101,7 @@ int main(int argc, char** argv)
 	muon::system::Log::registerLogImpl(&logFile);
 
 	muon::system::Log::open("unit_test.log");
-	muon::system::Log mainLog("Main", muon::LOG_DEBUG);
+	muon::system::Log mainLog("Main", muon::LOG_INFO);
 
 	muon::meta::MetaDatabase::instantiate();
 
@@ -93,15 +111,33 @@ int main(int argc, char** argv)
 		mainLog() << "\t: " << argv[i] << muon::endl;
 	}
 
+	muon::u32 errorCount = 0;
+
+#define MUON_TITLE(msg) mainLog() << msg << muon::endl
+#define MUON_CHECK(cond, err, ...) MUON_ASSERT(cond, "\t-> " err, __VA_ARGS__); if(!(cond)) {++errorCount;}
 	// ***************
 	// BEGIN UNIT TEST
+	void* pointer = NULL;
+	UnitTestObject* pUTO = NULL;
+
+	// Check that PoolAllocator correctly allocate, construct, destruct and free memory
+	// Check also that for allocated object A and B, if A is freed, then next will be allocated at A last place
+	MUON_TITLE(" ** Checking muon::memory::PoolAllocator ** ");
+	pUTO = muon::memory::PoolAllocator::allocate<UnitTestObject>(1);
+	MUON_CHECK(pUTO, "Could not allocate with PoolAllocator!");
+	pointer = muon::memory::PoolAllocator::construct(1, pUTO, {});
+	MUON_CHECK(pointer == pUTO, "PoolAllocator::construct returned a different address: %p != %p", pointer, pUTO);
+	MUON_CHECK(pUTO->value == UnitTestObject::ConstructedValue, "UnitTestObject not correctly constructed: %d != %d", pUTO->value, UnitTestObject::ConstructedValue);
+	muon::memory::PoolAllocator::destroy(pUTO, {});
+	MUON_CHECK(pUTO->value == UnitTestObject::DestructedValue, "UnitTestObject not correctly destroyed: %d != %d", pUTO->value, UnitTestObject::DestructedValue);
 
 	// END UNIT TEST
 	// ***************
 
+	mainLog(errorCount == 0 ? muon::LOG_INFO : muon::LOG_ERROR) << "Error Count: " << errorCount << muon::endl;
 	muon::system::Log::close();
 #ifdef MUON_PLATFORM_WINDOWS
 	::system("PAUSE");
 #endif
-	return 0;
+	return -((muon::i32)errorCount);
 }
